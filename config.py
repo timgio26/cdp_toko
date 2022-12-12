9ed5f69cd9c4f347439d2f5c18541e2a39c72473
nutrimartid <nutrimartid@gmail.com>
2022-12-12 15:16:12 +0700

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS ={'pool_pre_ping': True}
+SQLALCHEMY_POOL_RECYCLE=299
+SECRET_KEY='bumimarinaemas'
+# UPLOAD_FOLDER='static/upload'
+DEBUG=True
+ROW_PER_PAGE=15
