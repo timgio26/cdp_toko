be8f513ae248c8ff7e4a89ad45890a29dc033f20
timotius giovandi <tim.gio@outlook.com>
2025-11-14 09:31:26 +0100

    backend route not found redirect

--- backend/cdp_toko/__init__.py
+++ backend/cdp_toko/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from cdp_toko.extension import migrate,db,jwt
 from cdp_toko.models.models import UserCdp,  Customer, Service ##need to be migrated ?
 from cdp_toko.routes.routes import main_bp
+from cdp_toko.routes.error_handler import error_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     app.register_blueprint(main_bp)
+    app.register_blueprint(error_bp)
     jwt.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
